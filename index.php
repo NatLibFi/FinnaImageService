87842dda300057447d28c5313d97bf1f237b7491
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-09-23 15:51:23 +0300

    Increase logging and timeout, cleanup temporary files.

--- index.php
+++ index.php
@@ -35,36 +35,74 @@ if (!$url) {
     return;
 }
 
-$inputPath = tempnam(sys_get_temp_dir(), 'pdf2jpg-input');
-$outputPath = tempnam(sys_get_temp_dir(), 'pdf2jpg-output');
+if (!$inputPath = tempnam(sys_get_temp_dir(), 'pdf2jpg-input')) {
+    error_log("Failed to create temporary input file.");
+    return;
+}
 
 // Download PDF to a temporary file
 $client = new \Zend\Http\Client();
-$client->setOptions(array('strictredirects' => false));
+$client->setOptions(['strictredirects' => false, 'timeout' => 20]);
 $client->setStream($inputPath);
 $client->setUri($url);
-$result = $client->send();
+$client->setAdapter('Zend\Http\Client\Adapter\Curl');
+
+try {
+    $result = $client->send();
+} catch (\Exception $e) {
+    error_log("Failed to download pdf, url: $url");
+    error_log($e->getMessage());
+    unlink($inputPath);
+    return;
+}
 
 if (!$result->isSuccess() || !$result->getContentLength()) {
+    error_log(
+        "Error in downloaded pdf, content length: "
+        . $result->getContentLength() . ", url: $url"
+    );
+    unlink($inputPath);
+    return;
+}
+
+if (!$outputPath = tempnam(sys_get_temp_dir(), 'pdf2jpg-output')) {
+    unlink($inputPath);
+    error_log("Failed to create temporary output file.");
     return;
 }
 
 // Run Ghostscript
-$gs = sprintf('/usr/bin/gs -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=500000000 -dAlignToPixels=0 -dGridFitTT=2 -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -r150 -dFirstPage=1 -dLastPage=1 -sDEVICE=jpeg -o %s %s', $outputPath, $inputPath);
+$gs = sprintf(
+    '/usr/bin/gs -dQUIET -dSAFER -dBATCH -dNOPAUSE -dNOPROMPT -dMaxBitmap=500000000 -dAlignToPixels=0 -dGridFitTT=2 -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -r150 -dFirstPage=1 -dLastPage=1 -sDEVICE=jpeg -o %s %s',
+    $outputPath, $inputPath
+);
 exec($gs, $output, $returnVar);
 
+unlink($inputPath);
+
 if ($returnVar !== 0) {
+    error_log(
+        "Error executing Ghostscript, return status: {$returnVar}, url: {$url}"
+    );
+    unlink($outputPath);
     return;
 }
 
 // Output converted image
-$img = file_get_contents($outputPath);
+try {
+    $img = file_get_contents($outputPath);
+} catch (\Exception $e) {
+    error_log("Failed to read converted image: {$outputPath}, url: {$url}");
+    error_log($e->getMessage());
+
+    unlink($outputPath);
+    return;
+}
+
 header('Content-Type: image/jpeg');
 header('Content-Length: ' . strlen($img));
 
 echo $img;
-
-unlink($inputPath);
 unlink($outputPath);
 
 return;
