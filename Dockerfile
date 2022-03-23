d7a6c55f79cae606661af177f66bdfa4c00d6b84
LuomaJuha <juhaluoma5@gmail.com>
2022-03-23 15:03:08 +0200

    New version of service

--- Dockerfile
+++ Dockerfile
@@ -1,51 +1,19 @@
-FROM php:8.0-fpm
+FROM node:14.19.1
 
-# Install cron and nginx
-RUN apt-get -y update \
-    && apt-get -y install cron nginx libzip-dev
+# Create app directory
+WORKDIR /usr/src/app
 
-# Install opcache and zip extensions
-RUN docker-php-ext-install opcache
-RUN docker-php-ext-install zip
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json ./
 
-# Download Ghostscript
-WORKDIR /tmp
-RUN curl -L https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9550/ghostscript-9.55.0-linux-x86_64.tgz --output gs.tgz
-RUN tar zxvf gs.tgz
-RUN mv ghostscript-*-linux-x86_64/gs-*-linux-x86_64 /usr/bin/gs
+RUN npm install
+# If you are building your code for production
+# RUN npm ci --only=production
 
-# Copy files to www-root
-COPY index.php composer.json composer.lock /var/www/html/
-COPY src /var/www/html/src
+# Bundle app source
+COPY . .
 
-# Composer
-RUN curl -sS https://getcomposer.org/installer | php \
-    && mv composer.phar /usr/local/bin/composer \
-    && chmod a+x /usr/local/bin/composer
-
-RUN mkdir -p /var/www/html
-RUN chmod u+w /var/www/html
-RUN chown -R www-data:www-data /var/www/html/
-
-COPY nginx-default.conf /etc/nginx/sites-enabled/default
-
-# Create directories for images and status files
-RUN mkdir /tmp/pdf2jpg
-RUN mkdir /tmp/pdf2jpg/in
-RUN mkdir /tmp/pdf2jpg/out
-RUN mkdir /tmp/pdf2jpg/status
-RUN chown -R www-data:www-data /tmp/pdf2jpg
-
-WORKDIR /var/www/html
-USER www-data
-RUN composer --no-cache install
-
-USER root
-
-# Install crontab to prune cache
-RUN echo "0 4 * * 0 find /tmp/pdf2jpg/ -mtime +7 -type f -exec rm {} +" | crontab -
-
-# Start cron, php-fpm and nginx
-CMD service cron start \
-    && service nginx start \
-    && php-fpm -F
+EXPOSE 5000
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,197 @@
+import express from 'express';
+import MD5 from "crypto-js/md5.js";
+import fs from 'fs';
+import fetch from 'node-fetch';
+import path from 'path';
+import {fileURLToPath, URL} from 'url';
+import {PDFImage} from 'pdf-image';
+import winston from 'winston';
+
+const { combine, timestamp, printf } = winston.format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `{"${timestamp}" "${level}": "${message}"}`;
+});
+
+const port = 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const imagesDir = path.join(__dirname, 'images');
+const tmpDir = path.join(__dirname, 'tmp');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(
+    timestamp(),
+    myFormat
+  ),
+  defaultMeta: {service: 'pdf-to-image'},
+  transports: [
+    new winston.transports.File({filename: 'error.log', level: 'error'}),
+    new winston.transports.File({filename: 'info.log', level: 'info'})
+  ]
+});
+
+// If the process is not started as production, then log into console also.
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+    'timestamp': true
+  }));
+}
+
+// Be sure images directory exists
+if (!fs.existsSync(imagesDir)) {
+  fs.mkdirSync(imagesDir);
+}
+if (!fs.existsSync(tmpDir)) {
+  fs.mkdirSync(tmpDir);
+}
+
+const app = express();
+
+/**
+ * Download a file.
+ * 
+ * @param {string} fileUrl 
+ * @param {string} destPath 
+ */
+function downloadFile(fileUrl, destPath) {
+  if (!fileUrl) return Promise.reject(new Error('Invalid fileUrl'));
+  if (!destPath) return Promise.reject(new Error('Invalid destPath'));
+
+  return new Promise((resolve, reject) => {
+      fetch(fileUrl).then((res) => {
+          const fileStream = fs.createWriteStream(destPath);
+          res.body.on('error', reject);
+          fileStream.on('finish', () => {
+            resolve(true);
+          });
+          res.body.pipe(fileStream);
+      });
+  });
+}
+
+/**
+ * Send the file back to client
+ *
+ * @param {object} res 
+ * @param {string} file 
+ */
+function sendResult(res, file) {
+  res.download(file);
+  logger.info(`Image sent: ${file}`);
+}
+
+/**
+ * Remove file from the given path.
+ *
+ * @param {string} file 
+ */
+function removeFile(file) {
+  fs.unlink(file, err => {
+    if (err) {
+      logger.error(`Error removing file: ${file}. Reason: ${err.message}`);
+    }
+  });
+}
+
+/**
+ * If the given url is valid.
+ * 
+ * @param {string} s 
+ */
+const stringIsAValidUrl = (s) => {
+  try {
+    new URL(s);
+    return true;
+  } catch (err) {
+    logger.error(`URL is not valid: ${s}`);
+    return false;
+  }
+};
+
+/**
+ * Convert pdf to jpg and send status
+ *
+ * @param {string} source 
+ * @param {string} destination 
+ * @param {object} res 
+ */
+function convertPDFtoJpg(source, destination, res) {
+  const pdf = new PDFImage(source, {
+    convertOptions: {
+      "-define": "PDF:use-cropbox=true",
+      "-strip": ''
+    }
+  });
+  pdf.convertPage(0).then((savedFile) => {
+    if (fs.existsSync(savedFile)) {
+      fs.rename(savedFile, destination, (err) => {
+        fs.unlinkSync(source);
+        if (err) {
+          logger.error(`Error moving file: ${savedFile} > ${destination}. Reason: ${err.message}`);
+          res.sendStatus(500);
+        } else {
+          sendResult(res, destination);
+        }
+      });
+    }
+  }, (reason) => {
+    logger.error(`Failed to convert PDF into a jpg file. Reason: ${reason}`);
+  });
+}
+
+app.get('/convert', (req, res) => {
+  if (typeof req.query.url !== 'undefined' && stringIsAValidUrl(req.query.url)) {
+    const url = req.query.url;
+    const fileName = MD5(url);
+    const imagePath = `${imagesDir}/${fileName}.jpg`;
+    logger.info(`Convert request: ${url},${fileName},${imagePath}`);
+    if (!fs.existsSync(imagePath)) {
+      const tmpPath = `${tmpDir}/${fileName}.pdf`;
+      if (fs.existsSync(tmpPath)) {
+        logger.info(`PDF exists: ${url}`);
+        convertPDFtoJpg(tmpPath, imagePath, res);
+      } else {
+        const response = downloadFile(url, tmpPath).then(() => {
+          logger.info(`PDF download: ${url}`);
+          convertPDFtoJpg(tmpPath, imagePath, res);
+        });
+      }
+    } else {
+      sendResult(res, imagePath);
+    }
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.get('/clear', (req, res) => {
+  [tmpDir, imagesDir].forEach((dir) => {
+    fs.readdir(dir, (err, files) => {
+      if (err) {
+        logger.error(`Error reading directory: ${dir}. Reason: ${err.message}`);
+        throw err;
+      }
+      files.forEach((file) => {
+        const tar = path.join(dir, file);
+        fs.unlink(tar, error => {
+          if (error) {
+            logger.error(`Error removing file: ${tar}. Reason: ${error.message}`);
+          }
+        });
+      });
+    });
+  });
+});
+
+app.get('/status', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Image service now listening to port: ${port}`);
+});
